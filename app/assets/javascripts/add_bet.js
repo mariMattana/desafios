617bf04d015a4c90645147c01ed88f879d9ed2f3
Daniel Rodrigues <daniel.phr@gmail.com>
2018-02-04 19:02:57 -0200

    Modal Css

--- app/assets/javascripts/add_bet.js
+++ app/assets/javascripts/add_bet.js
@@ -1,10 +1,11 @@
+let modal = document.getElementById('add-bet-modal');
+
 let invite = document.getElementById("invite-user");
 invite.addEventListener("click", (event) => {
   modal.style.display = "block";
 });
 
-var modal = document.getElementById('modal-add-bet');
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-}
+let close = document.getElementById("modal-close");
+close.addEventListener("click", (event) => {
+  modal.style.display = "none";
+});
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,12 @@ User.destroy_all
 puts "Creating Users ..."
 users = [
   {email: "daniel.phr@gmail.com", password: "Luigi123", first_name: "Daniel", last_name: "Rodrigues", nickname: "danielphr", birthday: "13/06/1988", cpf: "111.111.111-11", url: "http://res.cloudinary.com/danielphr/image/upload/v1517412297/vr2bd99d59tdgziorbmz.jpg"},
-  {email: "pedro@pedro.com", password: "Mario123", first_name: "Pedro", last_name: "Borges", nickname: "pedrao", birthday: "01/01/1991", cpf: "222.222.222-22"},
-  {email: "mari@mari.com", password: "Peach123", first_name: "Mari", last_name: "Mattana", nickname: "mari", birthday: "02/02/1992", cpf: "333.333.333-33"}
+  {email: "pedro@pedro.com", password: "Mario123", first_name: "Pedro", last_name: "Borges", nickname: "pedrao", birthday: "01/01/1991", cpf: "222.222.222-22", url: "http://res.cloudinary.com/danielphr/image/upload/v1517776006/einsten_oimozu.jpg"},
+  {email: "mari@mari.com", password: "Peach123", first_name: "Mari", last_name: "Mattana", nickname: "mari", birthday: "02/02/1992", cpf: "333.333.333-33", url: "http://res.cloudinary.com/danielphr/image/upload/v1517420449/osv9o0h6y6obwgkrnpcf.jpg"}
 ]
 users.each do |user|
   puts "Entrei aqui"
  new_user = User.new(email: user[:email], password: user[:password], first_name: user[:first_name], last_name: user[:last_name], nickname: user[:nickname], birthday: user[:birthday], cpf: user[:cpf])
- # new_user.remote_photo_url = user[:url]
+ new_user.remote_photo_url = user[:url]
  new_user.save
 end
