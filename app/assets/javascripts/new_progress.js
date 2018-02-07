3ab5d2619d5754c1f09750c85019f22c2453f783
Mariana Mattana <mariana.dev2017@gmail.com>
2018-02-07 11:09:29 -0200

    inserting form at challenge_show

--- app/assets/javascripts/new_progress.js
+++ app/assets/javascripts/new_progress.js
@@ -1,19 +1,19 @@
-// $('#progress-form').submit(function(e){
-//   e.preventDefault();
+$('#progress-form').submit(function(e){
+  e.preventDefault();
 
-//   let progressItem = $(this).serialize();
+  let progressItem = $(this).serialize();
 
-//   $.post('/challenge/:id/show', progressItem, function(data){
-//     $('new-progress').append(
-//       `
-//       <h3>${data.title}</h3>
-//       <div class="progress-img">
-//         <%= cl_image_tag(progress.user.photo, width: 60, height: 60, crop: :fill) %>
-//       </div>
-//       <p>${data.user.first_name} + " " + ${data.user.last_name}</p>
-//       <p>${data.description}</p>
-//       <p>Postado em ${data.date}</p>
-//       `
-//       )
-//   })
-// })
+  $.post('/challenge/:id/show', progressItem, function(data){
+    $('new-progress').append(
+      `
+      <h3>${data.title}</h3>
+      <div class="progress-img">
+        <%= cl_image_tag(progress.user.photo, width: 60, height: 60, crop: :fill) %>
+      </div>
+      <p>${data.user.first_name} + " " + ${data.user.last_name}</p>
+      <p>${data.description}</p>
+      <p>Postado em ${data.date}</p>
+      `
+      )
+  })
+})
--- app/controllers/challenges_controller.rb
+++ app/controllers/challenges_controller.rb
@@ -11,6 +11,7 @@ class ChallengesController < ApplicationController
     end
     @challenge = Challenge.find(params[:id])
     @bets = Bet.where(challenge: @challenge)
+    @progress = Progress.new
     @progresses = Progress.where(challenge: @challenge)
     authorize @challenge
   end
