c0e3fcdec39cf323a6c0c9da034eaffa08929b66
Daniel Rodrigues <daniel.phr@gmail.com>
2018-02-07 16:53:14 -0200

    Ajusted Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Progress.destroy_all
 
 puts "Creating Users ..."
 users = [
-  {email: "daniel.phr@gmail.com", password: "Luigi123", first_name: "Daniel", last_name: "Rodrigues", nickname: "danielphr", birthday: "13/06/1988", cpf: "111.111.111-11", url: "http://res.cloudinary.com/danielphr/image/upload/v1517412297/vr2bd99d59tdgziorbmz.jpg"},
+  {email: "daniel.phr@gmail.com", password: "Luigi123", first_name: "Daniel", last_name: "Rodrigues", nickname: "danielphr", birthday: "13/06/1988", cpf: "111.111.111-11", url: "http://res.cloudinary.com/danielphr/image/upload/v1518025124/wa022cmdzxuacyybqyng.jpg"},
   {email: "pedro@pedro.com", password: "Mario123", first_name: "Pedro", last_name: "Borges", nickname: "pedrao", birthday: "01/01/1991", cpf: "222.222.222-22", url: "http://res.cloudinary.com/danielphr/image/upload/v1518024020/pedro_dv90pd.jpg"},
   {email: "mari@mari.com", password: "Peach123", first_name: "Mari", last_name: "Mattana", nickname: "mari", birthday: "02/02/1992", cpf: "333.333.333-33", url: "http://res.cloudinary.com/danielphr/image/upload/v1517420449/osv9o0h6y6obwgkrnpcf.jpg"},
   {email: "patricia@patricia.com", password: "Patricia", first_name: "Patricia", last_name: "Moura", nickname: "patty", birthday: "13/08/1992", cpf: "444.444.444-44", url: "http://res.cloudinary.com/danielphr/image/upload/v1518022396/sqigpp92jcsisehflsg0.jpg"},
@@ -62,7 +62,7 @@ progresses = [{
     title: "Uhu, emagreci 1 Kg",
     description: "testestetsestest\ntestestestetsetstestest\ntesteteststse",
     date: "15/02/2018",
-    url: "http://res.cloudinary.com/danielphr/image/upload/v1517776006/einsten_oimozu.jpg",
+    url: "http://res.cloudinary.com/danielphr/image/upload/v1518028356/sample.jpg",
     user_id: 1,
     challenge_id: 1
 }]
