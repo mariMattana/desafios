6e1c8e90ec840ac3cffeb28ae7b49d274835914b
Mariana Mattana <mariana.dev2017@gmail.com>
2018-02-07 15:31:17 -0200

    front progress form

--- app/assets/javascripts/new_progress.js
+++ app/assets/javascripts/new_progress.js
@@ -1,19 +1,26 @@
-$('#progress-form').submit(function(e){
-  e.preventDefault();
+function displayProgressForm() {
+  document.getElementById("progress-form").classList.toggle("hidden");
+}
 
-  let progressItem = $(this).serialize();
+function displayProgressForm() {
+  document.getElementById("progress-form").classList.toggle("hidden");
+}
+// $('#progress-form').submit(function(e){
+//   e.preventDefault();
 
-  $.post('/challenge/:id/show', progressItem, function(data){
-    $('new-progress').append(
-      `
-      <h3>${data.title}</h3>
-      <div class="progress-img">
-        <%= cl_image_tag(progress.user.photo, width: 60, height: 60, crop: :fill) %>
-      </div>
-      <p>${data.user.first_name} + " " + ${data.user.last_name}</p>
-      <p>${data.description}</p>
-      <p>Postado em ${data.date}</p>
-      `
-      )
-  })
-})
+//   let progressItem = $(this).serialize();
+
+//   $.post('/challenge/:id/show', progressItem, function(data){
+//     $('new-progress').append(
+//       `
+//       <h3>${data.title}</h3>
+//       <div class="progress-img">
+//         <%= cl_image_tag(progress.user.photo, width: 60, height: 60, crop: :fill) %>
+//       </div>
+//       <p>${data.user.first_name} + " " + ${data.user.last_name}</p>
+//       <p>${data.description}</p>
+//       <p>Postado em ${data.date}</p>
+//       `
+//       )
+//   })
+// })
