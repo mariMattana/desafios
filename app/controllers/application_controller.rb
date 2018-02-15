8957d07b36c96a0b481181e72f38f77f8a53358f
Mariana Mattana <mariana.dev2017@gmail.com>
2018-02-15 14:13:18 -0200

    errs page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :nickname, :cpf, :birthday, :photo])
   end
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   private
 
   def skip_pundit?
